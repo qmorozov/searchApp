0fe36edbc99b815bff0a685df4ee1a1b44c2e89f
Andrew Morozov <andr.morozov96@gmail.com>
2022-07-27 13:27:53 +0300

    fix loading components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route, HashRouter} from "react-router-dom";
 import { SearchPage, HomePage } from "./components";
 
 const App = () => {
     return (
         <>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<HomePage />}/>
                     <Route path="search" element={<SearchPage />}/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </>
     );
 };
